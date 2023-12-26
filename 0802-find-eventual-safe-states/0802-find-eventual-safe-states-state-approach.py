9a72db8560b39c90b7cf9eecf34296d2d3662f45
Gunjan Modi <er.gunjanmodi@gmail.com>
2023-12-26 11:10:14 +0530

    Time: 530 ms(97.48%), Space: 24.8 MB (18.47%)

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states-state-approach.py
@@ -0,0 +1,27 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        
+        NON_VISITED, SAFE, INSIDE_RECURSION = 0, 1, 2
+        n = len(graph)
+        result = []
+        color = [NON_VISITED for _ in range(n)]
+
+        def helper(node):
+            if color[node] == INSIDE_RECURSION: return False
+
+            if color[node] == SAFE: return True # This condition added just to prevent TLE for some use cases. Code can be submitted with this line.
+
+            color[node] = INSIDE_RECURSION
+
+            for adjacent in graph[node]:
+                if not helper(adjacent):
+                    return False
+
+            color[node] = SAFE
+            return True
+
+        for node in range(n):
+            if color[node] == SAFE or helper(node):
+                result.append(node)
+
+        return result
