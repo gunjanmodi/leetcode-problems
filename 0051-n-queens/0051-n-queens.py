1fba18e992217134196fd3b806d5da96b4b472e1
Gunjan Modi <er.gunjanmodi@gmail.com>
2024-01-04 13:48:30 +0530

    Time: 3344 ms (5.00%), Space: 18.2 MB (6.19%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,139 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        rows_range, cols_range = set(range(n)), set(range(n))
+
+        def backtrack(row, col, board, queen, combination):
+            if len(combination) == n:
+                model_combination = [["." for _ in range(n)] for _ in range(n)]
+                for x, y in combination:
+                    model_combination[x][y] = 'Q'
+
+                solution = []
+                for board_row in model_combination:
+                    solution.append(''.join(board_row))
+                result.append(solution)
+                return
+
+            for i in range(row, n):
+                for j in range(n):
+                    if len(board[i][j]) == 0:
+                        self.choose(i, j, queen, combination, board, rows_range, cols_range)
+                        backtrack(i, j, board, queen + 1, combination)
+                        self.unchoose(i, j, queen, combination, board, rows_range, cols_range)
+
+        board = [[[] for _ in range(n)] for _ in range(n)]
+        for column in range(n):
+            combination = []
+            self.choose(0, column, 1, combination, board, rows_range, cols_range)
+            backtrack(0, column, board, 2, combination)
+            self.unchoose(0, column, 1, combination, board, rows_range, cols_range)
+
+        return result
+
+
+    def choose(self, i, j, queen, combination, board, rows_range, cols_range):
+        combination.append((i, j))
+        board[i][j].append(queen)
+        # queen will be added to 8 directions
+        # Add queen to Top Left 
+        r, c = i - 1, j - 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r -= 1
+            c -= 1
+        # Add queen to Top
+        r, c = i - 1, j
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r -= 1
+        # Add queen to Top Right
+        r, c = i - 1, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r -= 1
+            c += 1
+        # Add queen to left
+        r, c = i, j - 1
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            c -= 1
+        # Add queen to right
+        r, c = i, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            c += 1
+        # Add queen to bottom Left
+        r, c = i + 1, j - 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r += 1
+            c -= 1
+        # Add queen to bottom
+        r, c = i + 1, j
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r += 1
+        # Add queen to bottom Right
+        r, c = i + 1, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].append(queen)
+            r += 1
+            c += 1
+    
+    def unchoose(self, i, j, queen, combination, board, rows_range, cols_range):
+        combination.pop()
+        board[i][j].pop()
+        # queen will be removed to 8 directions
+        # Remove queen from Top Left 
+        r, c = i - 1, j - 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r -= 1
+            c -= 1
+        # Remove queen from Top
+        r, c = i - 1, j
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r -= 1
+        # Remove queen from Top Right
+        r, c = i - 1, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r -= 1
+            c += 1
+        # Remove queen from left
+        r, c = i, j - 1
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            c -= 1
+        # Remove queen from right
+        r, c = i, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            c += 1
+        # Remove queen from bottom Left
+        r, c = i + 1, j - 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r += 1
+            c -= 1
+        # Remove queen from bottom
+        r, c = i + 1, j
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r += 1
+        # Remove queen from bottom Right
+        r, c = i + 1, j + 1 
+        while r in rows_range and c in cols_range:
+            board[r][c].pop()
+            r += 1
+            c += 1
+
+
+
+
+
+
+
+
