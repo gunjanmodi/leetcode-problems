dcbce3e3baa869d06b80c6b6a7ef64904aa22a22
Gunjan Modi <er.gunjanmodi@gmail.com>
2024-02-27 11:36:07 +0530

    Time: 5040 ms (5.03%), Space: 16.5 MB (93.69%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,20 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 1, n
+        result = 1
+        def can_be_answer(num):
+            remaining = n
+            for current in range(num, 0, -1):
+                remaining -= current
+                if remaining < 0:
+                    return False
+            return True
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if can_be_answer(mid):
+                result = mid
+                left = mid + 1
+            else:
+                right = mid
+        return result
