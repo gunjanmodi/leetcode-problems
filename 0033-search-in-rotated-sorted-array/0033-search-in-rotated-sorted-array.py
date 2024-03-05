4bed57566c52a8e9a22d5db3f653727de5bbdbb3
Gunjan Modi <er.gunjanmodi@gmail.com>
2024-03-05 12:33:19 +0530

    Updating 0033-search-in-rotated-sorted-array with improvisation

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,23 +4,23 @@ class Solution:
         left, right = 0, n - 1
 
         if nums[left] < nums[right]:
-             self.binary_search(nums, target, 0, n - 1)
+            return self.binary_search(nums, target, 0, n - 1)
 
+        min_index = self.find_min_index(nums, left, right)
+
+        if nums[0] <= target <= nums[min_index - 1]:
+            return self.binary_search(nums, target, 0 , min_index - 1)
+        else:
+            return self.binary_search(nums, target, min_index, n - 1)
+
+    def find_min_index(self, nums, left, right):
         while left < right:
             mid = (left + right) >> 1
             if nums[mid] > nums[right]:
                 left = mid + 1
             else:
                 right = mid
-
-        min_index = left
-
-        ans_from_left_part = self.binary_search(nums, target, 0 , min_index - 1)
-        ans_from_right_part = self.binary_search(nums, target, min_index, n - 1)
-
-        if ans_from_left_part == -1 and ans_from_right_part == -1:
-            return -1
-        return ans_from_left_part if ans_from_left_part != -1 else ans_from_right_part
+        return left
 
     def binary_search(self, nums, target, left, right):
         while left < right:
